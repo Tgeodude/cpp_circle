014eb9efc04c5d4243fcaeb727537c3c0ec42efa
Tojamura Geodude <tgeodude@mi-b4.msk.21-school.ru>
2022-11-02 20:49:27 +0300

    day one must fix

--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -2,19 +2,40 @@
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
 
+std::string string_toupper(std::string str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
+
 int main (void)
 {
     PhoneBook book;
     int count = 0;
     std:: string name;
-    std:: cout << "Welcome, it's PhoneBook" << std:: endl;
+    std::cout << std::setw(50) << "\033[1;32mWelcome to Phonebook!\n \033[0m";
     while (1)
     {
+        std::cout << "\nEnter command: ADD, SEARCH or EXIT" << std::endl;
         std:: getline(std:: cin, name);
+        if (std::cin.eof() != 0)
+			exit (0);
+        name = string_toupper(name);
         if (name == "ADD")
         {   
-            book.Add(count);
-            count++;
+            if (count >= 8)
+				std::cout << "Phone book is full!" << std::endl;
+            else
+            {
+                book.Add(count);
+                count++;
+            }
         }
         else if (name == "SEARCH")
 			book.Search(count);
---
+++ day01/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+int main (void){
+
+	Zombie test("Foo");
+	test.announce();
+
+	return(0);
+}
